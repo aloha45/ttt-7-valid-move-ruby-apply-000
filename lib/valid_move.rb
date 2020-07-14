00a761f6c49742bfd501c02749870744129d54f0
Cory Spicer <corys4139@gmail.com>
2020-07-14 00:49:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,9 +12,7 @@ end
 
 
 def valid_move?(board, index)
-  if index.between?(0, 8) && !position_taken?
-    return false
-  elsif board[index] == "X" || board[index] == "O"
+  if board[index] == "X" || board[index] == "O"
     return false
   else
     return true
