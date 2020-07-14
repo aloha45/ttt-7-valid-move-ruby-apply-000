0274be40588ec9eef860f4db157dec33198cae4a
Cory Spicer <corys4139@gmail.com>
2020-07-14 00:36:06 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,10 +5,8 @@
 def position_taken?(board,index)
   if board[index] == "X" || board[index] == "O"
     return true
-  if board[index] == "" || board[index] == " " || board[index] == nil
-    return false
   else
-    return true
+    return false
   end
 end
 
